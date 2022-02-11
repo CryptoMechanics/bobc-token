f8cebf2140997ea7152735cbc896599339f57726
elfo <enginestart2@gmail.com>
2022-02-11 14:32:27 +0000

    infrastructure

--- contracts/BOBC.sol
+++ contracts/BOBC.sol
@@ -9,9 +9,9 @@ pragma solidity ^0.8.0;
 import "../@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-// 3330000000000000000000000
+// 6670000000000000000000000
 contract BOBC is ERC20{
     constructor() ERC20("BOBC", "BOBC")   {
-        _mint(msg.sender, 3330000000000000000000000);
+        _mint(msg.sender, 6670000000000000000000000);
     }
 }
