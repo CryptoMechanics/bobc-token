6892f50c5cd8ce24cea01f2bb4a9b937e3be5470
elfo <enginestart2@gmail.com>
2022-03-02 10:15:58 +0000

    removed comment

--- contracts/BOBC.sol
+++ contracts/BOBC.sol
@@ -1,11 +1,5 @@
 pragma solidity ^0.8.0;
 
-//its a simple example of a coin-like contract. have to read on ux contract
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers! madi
-
-
 import "../@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
